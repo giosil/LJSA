e84cfb5a32689666d566867fac95c0e4382141eb
Giorgio Silvestris <giorgio.silvestris@gmail.com>
2021-10-21 16:26:17 +0200

    update

--- src/test/java/org/dew/test/TestLJSA.java
+++ src/test/java/org/dew/test/TestLJSA.java
@@ -1,19 +1,63 @@
 package org.dew.test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.util.Properties;
 
-public class TestLJSA extends TestCase {
-  
-  public TestLJSA(String testName) {
-    super(testName);
-  }
-  
-  public static Test suite() {
-    return new TestSuite(TestLJSA.class);
-  }
-  
-  public void testApp() throws Exception {
+import org.dew.ljsa.backend.util.BEConfig;
+
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.JobKey;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerFactory;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+
+import org.quartz.impl.StdSchedulerFactory;
+
+public 
+class TestLJSA 
+{  
+  public static void main(String args[]) {
+    try {
+      System.out.println("Load config...");
+      Properties config = BEConfig.loadProperties("quartz.cfg");
+      
+      System.out.println("Create SchedulerFactory instance...");
+      SchedulerFactory schedulerFactory = new StdSchedulerFactory(config);
+      
+      System.out.println("Create Scheduler instance...");
+      Scheduler scheduler = schedulerFactory.getScheduler();
+      
+      JobDetail jobDetail = JobBuilder.newJob(TestJob.class)
+          .withIdentity("TestJob", "Group")
+          .storeDurably(true)
+          .build();
+      
+      scheduler.addJob(jobDetail, true);
+      
+      Trigger triggerS = TriggerBuilder.newTrigger()
+          .forJob(new JobKey("TestJob", "Group"))
+          .withIdentity("Simple", "Group")
+          .withSchedule(SimpleScheduleBuilder.simpleSchedule().repeatForever().withIntervalInMilliseconds(2 * 1000))
+          .build();
+      
+      scheduler.scheduleJob(triggerS);
+      
+      Trigger triggerC = TriggerBuilder.newTrigger()
+          .forJob(new JobKey("TestJob", "Group"))
+          .withIdentity("Cron", "Group")
+          .withSchedule(CronScheduleBuilder.cronSchedule("0/5 * * * * ?"))
+          .build();
+      
+      scheduler.scheduleJob(triggerC);
+      
+      System.out.println("Scheduler.start()...");
+      scheduler.start();
+    }
+    catch(Exception ex) {
+      ex.printStackTrace();
+    }
   }
 }
