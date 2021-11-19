794fe30349203a1d6c643b68380d22207f5327a6
Giorgio Silvestris <giorgio.silvestris@gmail.com>
2021-11-19 11:07:55 +0100

    update

--- gui/run-LJSA-gui.cmd
+++ gui/run-LJSA-gui.cmd
@@ -1,1 +1,15 @@
-"%JAVA_HOME%"\bin\java -cp target/LJSA-gui-2.0.0.jar;lib/swing3rd.jar;lib/swingup-1.0.0.jar org.dew.swingup.main.Main
+@echo off
+
+if "%JAVA_HOME%"=="" goto NOJAVAHOME
+
+set _CP=target/LJSA-gui-2.0.0.jar;lib/swing3rd.jar;lib/swingup-1.0.0.jar
+
+"%JAVA_HOME%"\bin\java -cp %_CP% org.dew.swingup.main.Main
+
+goto DONE
+
+:NOJAVAHOME
+echo JAVA_HOME not setted.
+goto DONE
+
+:DONE
